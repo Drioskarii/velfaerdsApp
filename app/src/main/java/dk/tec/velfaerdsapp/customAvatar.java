57b372d97ec56ea23767cfc51906f6a2c10b3ab7
Filip Jessen <filipjessen@gmail.com>
2020-10-30 14:07:56 +0100

    Indsat animation Placeholder

--- app/src/main/java/dk/tec/velfaerdsapp/customAvatar.java
+++ app/src/main/java/dk/tec/velfaerdsapp/customAvatar.java
@@ -2,17 +2,32 @@ package dk.tec.velfaerdsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class customAvatar extends AppCompatActivity {
+
+    AnimationDrawable animation;
+    ImageView characterPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_avatar);
+
+        characterPlaceholder = findViewById(R.id.characterPlaceholder);
+        characterPlaceholder.setBackgroundResource(R.drawable.animation);
+        animation = (AnimationDrawable) characterPlaceholder.getBackground();
+
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        animation.start();
+
     }
 }
 
-// avatar skal ind her
-// hej
-// github over slack
