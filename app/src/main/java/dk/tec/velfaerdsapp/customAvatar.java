bbe07746cd8e210be8eb5aece3c523d3bf7a77b4
Filip Jessen <filipjessen@gmail.com>
2020-11-05 08:28:47 +0100

    Animation works now

--- app/src/main/java/dk/tec/velfaerdsapp/customAvatar.java
+++ app/src/main/java/dk/tec/velfaerdsapp/customAvatar.java
@@ -3,15 +3,25 @@ package dk.tec.velfaerdsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class customAvatar extends AppCompatActivity {
+
+    AnimationDrawable animation;
+    ImageView characterPlaceholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_avatar);
+
+        characterPlaceholder = findViewById(R.id.characterPlaceholder);
+        characterPlaceholder.setBackgroundResource(R.drawable.animation);
+        animation = (AnimationDrawable) characterPlaceholder.getBackground();
+
     }
 
     public void forward(View view) {
@@ -24,5 +34,13 @@ public class customAvatar extends AppCompatActivity {
         Intent intent = new Intent(this, introPage.class);
 
         startActivity(intent);
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        animation.start();
+
     }
 }
