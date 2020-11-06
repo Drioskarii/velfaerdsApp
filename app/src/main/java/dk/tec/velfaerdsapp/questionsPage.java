bc66b8d5f52cea1e69f7fbcf4c13e5694201e267
Filip Jessen <filipjessen@gmail.com>
2020-11-06 07:52:01 +0100

    recycler View works, but scroller don't yet

--- app/src/main/java/dk/tec/velfaerdsapp/questionsPage.java
+++ app/src/main/java/dk/tec/velfaerdsapp/questionsPage.java
@@ -1,62 +1,78 @@
 package dk.tec.velfaerdsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.SeekBar;
-import android.widget.TableLayout;
-import android.widget.TextView;
+import android.widget.ImageView;
 
-import com.google.android.material.slider.Slider;
+//import com.google.android.material.slider.Slider;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class questionsPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_page);
 
-        String[] questionArray = {"One", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four"};
-        Object answersToQuestions = new Object();
-
-        LinearLayout llContainer = (LinearLayout) findViewById(R.id.llContainer);
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setGravity(LinearLayout.TEXT_ALIGNMENT_CENTER);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-
-        TableLayout.LayoutParams params = new TableLayout.LayoutParams();
 
 
-        params.setMargins(5, 5, 5, 100);
-        for( int i = 0; i < questionArray.length; i++ )
-        {
-            //button are created
-            TextView etq = new TextView(this);
-            SeekBar slider = new SeekBar(this);
-            TextView fisk = new TextView(this);
+        // questions list
+        ArrayList<questionboxes> questionList = new ArrayList<>();
+        questionList.add(new questionboxes(R.drawable.intellectual, "Question 1"));
+        questionList.add(new questionboxes(R.drawable.people_person, "Question 2"));
+        questionList.add(new questionboxes(R.drawable.sharp_brained, "Question 3"));
+        questionList.add(new questionboxes(R.drawable.intellectual, "Question 4"));
+        questionList.add(new questionboxes(R.drawable.people_person, "Question 5"));
+        questionList.add(new questionboxes(R.drawable.sharp_brained, "Question 6"));
 
-            //This is where editText the parameter goes.
-            etq.setText(questionArray[i]);
-            etq.setGravity(Gravity.CENTER);
-            fisk.setLayoutParams(params);
+        RecyclerView mQuestionRecyclerView = findViewById(R.id.recyclerView);
+        mQuestionRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        RecyclerView.Adapter mAdapter = new questionViewAdapter(questionList);
 
-            //This is where SeekBar the parameter goes.
+        mQuestionRecyclerView.setLayoutManager(mLayoutManager);
+        mQuestionRecyclerView.setAdapter(mAdapter);
 
 
-            //This is where the EditText gets added to activity_email_page.xml
-            llContainer.addView(etq);
-            llContainer.addView(slider);
-
-        }
+//        String[] questionArray = {"One", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four", "Two", "Three", "Four"};
+//        Object answersToQuestions = new Object();
+//
+//        LinearLayout llContainer = (LinearLayout) findViewById(R.id.llContainer);
+//        LinearLayout linearLayout = new LinearLayout(this);
+//        linearLayout.setGravity(LinearLayout.TEXT_ALIGNMENT_CENTER);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//
+//        TableLayout.LayoutParams params = new TableLayout.LayoutParams();
+//
+//
+//        params.setMargins(5, 5, 5, 100);
+//        for( int i = 0; i < questionArray.length; i++ )
+//        {
+//            //button are created
+//            TextView etq = new TextView(this);
+//            SeekBar slider = new SeekBar(this);
+//            TextView fisk = new TextView(this);
+//
+//            //This is where editText the parameter goes.
+//            etq.setText(questionArray[i]);
+//            etq.setGravity(Gravity.CENTER);
+//            fisk.setLayoutParams(params);
+//
+//            //This is where SeekBar the parameter goes.
+//
+//
+//            //This is where the EditText gets added to activity_email_page.xml
+//            llContainer.addView(etq);
+//            llContainer.addView(slider);
+//
+//        }
     }
     public void forward(View view) {
         Intent intent = new Intent(this, selectPage.class);
