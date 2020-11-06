f1daea70e2abb2598983e7339a895fdd1b8d5c53
Drioskarii <kagekrigeren@gmail.com>
2020-11-06 11:58:14 +0100

    Scuffed Cursor Wheel :) men den virker

---
+++ app/src/main/java/data/MenuItemData.java
@@ -0,0 +1,9 @@
+package data;
+
+public class MenuItemData {
+    public String mTitle;
+
+    public MenuItemData(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
--- app/src/main/java/dk/tec/velfaerdsapp/selectPage.java
+++ app/src/main/java/dk/tec/velfaerdsapp/selectPage.java
@@ -1,18 +1,49 @@
 package dk.tec.velfaerdsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
-public class selectPage extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Adapter.wheelCycleAdapter;
+import data.MenuItemData;
+import github.hellocsl.cursorwheel.CursorWheelLayout;
+
+public class selectPage extends AppCompatActivity implements CursorWheelLayout.OnMenuSelectedListener{
+    CursorWheelLayout wheel_text;
+    List<MenuItemData> lstText ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_page);
+
+
+        initViews();
+        loadData();
+
+        wheel_text.setOnMenuSelectedListener(this);
     }
+
+    private void loadData() {
+        lstText = new ArrayList<>();
+        for (int i = 0;i < 9;i++)
+            lstText.add(new MenuItemData("" + i));
+        lstText.add(new MenuItemData("OFF"));
+        wheelCycleAdapter adapter = new wheelCycleAdapter(getBaseContext(), lstText);
+        wheel_text.setAdapter(adapter);
+
+    }
+
+    private void initViews(){
+        wheel_text = (CursorWheelLayout) findViewById(R.id.wheel_text);
+}
+
 
     public void forward(View view) {
         Intent intent = new Intent(this, emailPage.class);
@@ -25,4 +56,15 @@ public class selectPage extends AppCompatActivity {
 
         startActivity(intent);
     }
+
+
+    @Override
+    public void onItemSelected(CursorWheelLayout parent, View view, int pos) {
+        if(parent.getId() == R.id.wheel_text);
+            Toast.makeText(getBaseContext(), "Selected: "+
+                    lstText.get(pos), Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
