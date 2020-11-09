1337b3527e7043eaafa9e8e662e57202f85d0072
nichlasmunch <47030396+nichlasmunch@users.noreply.github.com>
2020-11-09 12:29:44 +0100

    Byttet TEC og VfVOE logoerne, og renset noget kode

--- app/src/main/java/dk/tec/velfaerdsapp/MainActivity.java
+++ app/src/main/java/dk/tec/velfaerdsapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         startActivity(intent);
     }
-    public void strenghts(View view) {
+    public void strengths(View view) {
         Intent intent = new Intent(this, the24Strenght.class);
 
         startActivity(intent);
@@ -35,11 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
         switch(view.getId())
         {
-            case R.id.imgLogoTec:
-                url = "https://www.tec.dk/";
-                break;
             case R.id.imgLogoVfvoe:
                 url = "https://videnscenterportalen.dk/vtoe/";
+                break;
+            case R.id.imgLogoTec:
+                url = "https://www.tec.dk/";
                 break;
             case R.id.imgLogoUg:
                 url = "https://www.ug.dk/";
