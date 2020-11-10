cd4e99a56942c5ebdcfbf226eaba23dda2696f1a
nichlasmunch <47030396+nichlasmunch@users.noreply.github.com>
2020-11-10 12:21:37 +0100

    Udskiftet switch case med if/else ved behandling af view identifiers

--- app/src/main/java/dk/tec/velfaerdsapp/MainActivity.java
+++ app/src/main/java/dk/tec/velfaerdsapp/MainActivity.java
@@ -7,47 +7,48 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
-    public void forward(View view) {
+
+    public void forward(View view)
+    {
         Intent intent = new Intent(this, introPage.class);
 
         startActivity(intent);
     }
-    public void strengths(View view) {
+
+    public void strengths(View view)
+    {
         Intent intent = new Intent(this, the24Strenght.class);
 
         startActivity(intent);
     }
 
-    public void logoClicked(View view){
+    public void logoClicked(View view)
+    {
         String url = "";
+        int id = view.getId();
 
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
 
-        switch(view.getId())
-        {
-            case R.id.imgLogoVfvoe:
-                url = "https://videnscenterportalen.dk/vtoe/";
-                break;
-            case R.id.imgLogoTec:
-                url = "https://www.tec.dk/";
-                break;
-            case R.id.imgLogoUg:
-                url = "https://www.ug.dk/";
-                break;
-        }
+        if(id == R.id.imgLogoVfvoe)
+            url = "https://videnscenterportalen.dk/vtoe/";
+        else if (id == R.id.imgLogoTec)
+            url = "https://www.tec.dk/";
+        else if (id == R.id.imgLogoUg)
+            url = "https://www.ug.dk/";
 
         intent.setData(Uri.parse(url));
         startActivity(intent);
     }
-
 }
