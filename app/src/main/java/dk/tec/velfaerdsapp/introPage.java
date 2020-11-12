c052fea4da4b118b44b61c1ed1e590583d9a32f3
Daniel <danielp2004@msn.com>
2020-11-12 14:45:41 +0100

    Custom ExoPlayer implementeret til intro page
    - Bugs (#Arbejde jeg ikke gider lige nu, og venter til imorgen)
    Underlig handling når man trykker pause
    Fullscreen åbner ikke videoen op på skærmen, den sætter bare skærmen i landscape mode
    
    - Missing
    Spinner mangler en pæn custom dropdown.

--- app/src/main/java/dk/tec/velfaerdsapp/introPage.java
+++ app/src/main/java/dk/tec/velfaerdsapp/introPage.java
@@ -1,19 +1,30 @@
 package dk.tec.velfaerdsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.os.IResultReceiver;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,14 +38,32 @@ import android.widget.VideoView;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.RawResourceDataSource;
+import com.google.android.exoplayer2.util.Util;
 
 public class introPage extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
@@ -45,31 +74,167 @@ public class introPage extends AppCompatActivity implements GestureDetector.OnGe
     private EditText enterName;
     private EditText enterJob;
     private VideoView videoView;
+    private static Context context;
+
     String selectedItem = "";
+    PlayerView playerView;
+    ImageView fullscreenButton;
+    SimpleExoPlayer exoplayer;
+    boolean fullscreen = false;
+    FrameLayout.LayoutParams paramsNotFullscreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_page);
-
-        // instantiating
         enterName = findViewById(R.id.editTextName);
         enterJob = findViewById(R.id.editTextJob);
-        videoView = findViewById(R.id.videoView);
+        playerView = (PlayerView)findViewById(R.id.player_view);
+        fullscreenButton = findViewById(R.id.exo_fullscreen_icon);
 
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.styrker_video;
-        Uri uri = Uri.parse(videoPath);
-        videoView.setVideoURI(uri);
 
-        MediaController mediaController = new MediaController(this);
-        videoView.setMediaController(mediaController);
-        mediaController.setAnchorView(videoView);
-        videoView.setFocusable(true);
+       // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
+        //        , WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+       // LoadControl loadControl = new DefaultLoadControl();
+       // BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+       // TrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory(bandwidthMeter));
+
+       // exoplayer = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(introPage.this), new DefaultTrackSelector(), new DefaultLoadControl());
+
+       // String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.styrker_video;
+      //  Uri videoUrl = Uri.parse(videoPath);
+
+        //DefaultHttpDataSourceFactory factory = new DefaultHttpDataSourceFactory("exoplayer_video");
+       //ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+       //MediaSource mediaSource = new ExtractorMediaSource(videoUrl, factory, extractorsFactory, null, null);
+
+
+
+       // playerView.setPlayer(exoplayer);
+      //  playerView.setKeepScreenOn(true);
+       // exoplayer.prepare(audioSource);
+       // exoplayer.setPlayWhenReady(true);
+
+
+        exoplayer= ExoPlayerFactory.newSimpleInstance(this,new DefaultTrackSelector());
+        DefaultDataSourceFactory defaultDataSourceFactory=new DefaultDataSourceFactory(this, Util.getUserAgent(this,"velfaerdsapp"));
+        exoplayer.setPlayWhenReady(true);
+        ExtractorMediaSource extractorMediaSource = new ExtractorMediaSource.Factory(defaultDataSourceFactory).createMediaSource(RawResourceDataSource.buildRawResourceUri(R.raw.styrker_video));
+        exoplayer.prepare(extractorMediaSource);
+        playerView.setPlayer(exoplayer);
+        playerView.setKeepScreenOn(true);
+        exoplayer.setPlayWhenReady(true);
+
+
+
+        exoplayer.addListener(new Player.EventListener() {
+            @Override
+            public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+            }
+
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+            }
+
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+
+            }
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+
+            }
+
+            @Override
+            public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+
+            }
+
+            @Override
+            public void onPositionDiscontinuity(int reason) {
+
+            }
+
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+            }
+
+            @Override
+            public void onSeekProcessed() {
+
+            }
+        });
+
+        fullscreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (fullscreen) {
+                    fullscreenButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen_open));
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                    fullscreen = false;
+                } else {
+                    fullscreenButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_fullscreen_close));
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                    fullscreen = true;
+                }
+            }
+        });
+
+
+
+
+
+        // videoView = findViewById(R.id.videoView);
+
+       // String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.styrker_video;
+        //Uri uri = Uri.parse(videoPath);
+        //videoView.setVideoURI(uri);
+
+       // MediaController mediaController = new MediaController(this);
+       // videoView.setMediaController(mediaController);
+      //  mediaController.setAnchorView(videoView);
+       // videoView.setFocusable(true);
+       // setVolumeControlStream(AudioManager.STREAM_MUSIC);
         // Calling the method to create the spinnerdropdown
-        createSpinnerDropdown();
+       // createSpinnerDropdown();
 
         //init gestureDetector
         this.gestureDetector = new GestureDetector(introPage.this, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        exoplayer.setPlayWhenReady(false);
+        exoplayer.getPlaybackState();
+
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        exoplayer.setPlayWhenReady(true);
+        exoplayer.getPlaybackState();
     }
 
     @Override
@@ -96,6 +261,8 @@ public class introPage extends AppCompatActivity implements GestureDetector.OnGe
         }
         return super.onTouchEvent(event);
     }
+
+
 
     public void forward() {
         Intent intent = new Intent(introPage.this, customAvatar.class);
@@ -178,5 +345,29 @@ public class introPage extends AppCompatActivity implements GestureDetector.OnGe
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         return false;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+
+
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) //To fullscreen
+        {
+            paramsNotFullscreen=(FrameLayout.LayoutParams)playerView.getLayoutParams();
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(paramsNotFullscreen);
+            params.setMargins(0, 0, 0, 0);
+            params.height= ViewGroup.LayoutParams.MATCH_PARENT;
+            params.width=ViewGroup.LayoutParams.MATCH_PARENT;
+            //params.addRule(FrameLayout.CENTER_IN_PARENT);
+            playerView.setLayoutParams(params);
+
+        }
+        else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+        {
+            playerView.setLayoutParams(paramsNotFullscreen);
+        }
     }
 }
