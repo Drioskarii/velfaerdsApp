fd942ab94f172407f6d8ff039a7b1865695f085c
Rasmus <Rasmusstaal.tec@gmail.com>
2020-12-04 13:32:15 +0100

    fixes to mainpage animation, displays tutorial only once

--- app/src/main/java/dk/tec/velfaerdsapp/MainActivity.java
+++ app/src/main/java/dk/tec/velfaerdsapp/MainActivity.java
@@ -19,38 +19,51 @@ public class MainActivity extends TouchActivityHandler {
 
     private static final String TAG = "mainActivity";
     final Handler handler = new Handler();
-    boolean tutDone = false;
+    Runnable myRunnable;
+    boolean tutDone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                tutorialFrame();
-            }
-        }, 0);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                tutorialText();
-            }
-        }, 5000);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                tutDone = true;
-                tutorialFrame();
-            }
-        }, 8000);
-
         SharedPreferences settings1 = getSharedPreferences("introValues", Context.MODE_PRIVATE);
         settings1.edit().clear().apply();
         SharedPreferences settings2 = getSharedPreferences("questionArray", Context.MODE_PRIVATE);
         settings2.edit().clear().apply();
-        SharedPreferences settings3 = getSharedPreferences("emailArray", Context.MODE_PRIVATE);
+        SharedPreferences settings3= getSharedPreferences("selectArray", Context.MODE_PRIVATE);
         settings3.edit().clear().apply();
+        SharedPreferences settings4 = getSharedPreferences("emailArray", Context.MODE_PRIVATE);
+        settings4.edit().clear().apply();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("tutorialState", MODE_PRIVATE);
+        String s1 = sharedPreferences.getString("tutorialState", "");
+        tutDone = s1.contains("true");
+        if (!tutDone) {
+            tutorialFrame();
+            handler.postDelayed(myRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    tutorialText();
+                }
+            }, 4000);
+            handler.postDelayed(myRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    tutDone = true;
+                    tutorialFrame();
+                }
+            }, 8000);
+            handler.postDelayed(myRunnable = new Runnable() {
+                @Override
+                public void run() {
+                    RelativeLayout relativeLayout = findViewById(R.id.layoutTutorial);
+                    relativeLayout.setVisibility(View.GONE);
+                    SharedPreferences sharedPref = getSharedPreferences("tutorialState", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("tutorialState", ""+tutDone);
+                    editor.apply();
+                }
+            }, 9000);
+        }
     }
 
     public void logoClicked(View view) {
