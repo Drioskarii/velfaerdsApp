6907e215aa00066e532580309a530e28849212f5
RasmusTec <60211912+RasmusTec@users.noreply.github.com>
2021-02-01 08:21:38 +0100

    quick fix

--- app/src/main/java/dk/tec/velfaerdsapp/IntroPage.java
+++ app/src/main/java/dk/tec/velfaerdsapp/IntroPage.java
@@ -35,7 +35,6 @@ public class IntroPage extends TouchActivityHandler {
     Button btnBack, btnForward;
     private EditText enterName;
     private EditText enterJob;
-    String selectedItem = "";
     Spinner spinnerGender;
 
     @Override
